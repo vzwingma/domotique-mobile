e1411c3675546872200e31677ead5a687768a03f
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2024-08-24 23:42:53 +0200

    import

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren, ReactElement } from 'react';
-import { RefreshControl, StyleSheet, useColorScheme } from 'react-native';
+import { RefreshControl, StyleSheet } from 'react-native';
 import Animated, {
   interpolate,
   useAnimatedRef,
