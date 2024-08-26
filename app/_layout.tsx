38d0944df9d01a1cfff542b205bf13d256cb5a69
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2024-08-26 15:20:55 +0200

    refactor: Remove unused code and update dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DarkTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+/**
+ * Layout de base
+ * @returns  {JSX.Element} Layout de base
+ */
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
