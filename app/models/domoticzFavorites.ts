cefb24414e5ec028c24e729c085a621f08e93946
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2024-09-26 22:14:22 +0200

    correction du tri

--- app/models/domoticzFavorites.ts
+++ app/models/domoticzFavorites.ts
@@ -8,13 +8,14 @@ class DomoticzFavorites {
     // Index de l'équipement
     readonly idx: number;
     // Nombre de fois où l'équipement est activé
-    public favorites: number;
+    public nbOfUse: number;
     // Nom de l'équipement
     readonly name: string;
     // Type de l'équipement
     readonly type: DomoticzType;
     // Sous-type de l'équipement (Lumière, volet, ...)
     readonly subType: string;
+    public rang: number = 0;
 
     /**
      * Constructeur
@@ -24,14 +25,14 @@ class DomoticzFavorites {
      * 
      * @param {DomoticzFavorites} options - L'objet options contenant les propriétés pour l'initialisation.
      * @param {number} options.idx - L'index des favoris.
-     * @param {number} options.favourites - Le nombre de fois où les favoris sont activés.
+     * @param {number} options.nbOfUse - Le nombre de fois où les favoris sont activés.
      * @param {string} options.name - Le nom des favoris.
      * @param {DomoticzType} options.type - Le type des favoris.
      * @param {string} options.subType - Le sous-type des favoris.
      */
-    constructor({ idx, favorites, name, type, subType }: DomoticzFavorites) {
+    constructor({ idx, nbOfUse, name, type, subType }: DomoticzFavorites) {
         this.idx = idx;
-        this.favorites = favorites;
+        this.nbOfUse = nbOfUse;
         this.name = name;
         this.type = type;
         this.subType = subType;
